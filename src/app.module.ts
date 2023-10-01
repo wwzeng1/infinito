6b6c98ffacb5ba9f40b3f8eb743b9f2f74a0c722
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-01 18:37:46 -0300

    auth generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserController } from './controllers/user.controller';
 import UserService from "./services/user.service";
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
