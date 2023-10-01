ff84dc1274474b6a6109c658e7f178e5dcb9a622
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-01 16:47:05 -0300

    asdf

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserController } from './controllers/user.controller';
-import { UserService } from './services/user.service';
+import UserService from "./services/user.service";
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
