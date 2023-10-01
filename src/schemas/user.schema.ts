59fe567d9deab9fe2a77bb6314336b6c1f5464e1
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-01 19:29:56 -0300

    asdf

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,10 +1,10 @@
 import * as mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  email: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
-  dob: { type: Date, required: true },
+  dateofbirth: { type: Date, required: true },
   dateOfRegistry: { type: Date, default: Date.now },
   profession: { type: String, required: true },
   rut: { type: String, required: true },
