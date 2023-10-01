9f96c34643465d59f83b0dddc5195b5d618313c6
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-01 19:12:25 -0300

    asdf

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule,JwtService } from '@nestjs/jwt';
 import { JWT_SECRET } from './constants';
 
 @Module({
