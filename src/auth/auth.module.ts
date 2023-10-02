0c2d2ab176d0f282808a1c5d39358566f54603fb
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-02 01:17:32 -0300

    todo funcional jeje

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,19 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule, JwtService } from '@nestjs/jwt'; // Added import statement for JwtService
-import { UserService } from './user.service'; // Added import statement for UserService
-
-@Module({
-  imports: [
-    JwtModule.register({
-      secret: JWT_SECRET,
-    }),
-    PassportModule,
-  ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtService, UserService, JwtStrategy] // Added JwtService and UserService to the providers array
-})
-export class AuthModule {}
+import  UserService  from '../services/user.service'; // Added import statement for UserService
 import { JWT_SECRET } from './constants';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
@@ -27,6 +15,6 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtService, JwtStrategy]
+  providers: [AuthService, JwtService, UserService, JwtStrategy] // Added JwtService and UserService to the providers array
 })
 export class AuthModule {}
