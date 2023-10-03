62955f47b1e80cbbf88311f239f9f13add4b978e
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-03 14:46:39 -0300

    afd

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   name: { type: String, required: true },
+  rol: {type: String, default:'Usuario'},
   password: { type: String, required: true },
   dateofbirth: { type: Date, required: true },
   dateOfRegistry: { type: Date, default: Date.now },
